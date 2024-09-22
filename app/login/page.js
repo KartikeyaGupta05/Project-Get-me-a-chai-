d977c1920dc7e2f09948700faf6a336dd9b59c9f
Kartikeyagupta5 <kartikeyagupta998@gmail.com>
2024-09-22 14:05:12 +0530

    Update login

--- app/login/page.js
+++ app/login/page.js
@@ -3,7 +3,7 @@ import React, {useEffect} from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 import { useRouter } from 'next/navigation'
 
-const login = () => {
+const Login = () => {
   const { data: session } = useSession()
   // if(session) {
   //       const router = useRouter()
