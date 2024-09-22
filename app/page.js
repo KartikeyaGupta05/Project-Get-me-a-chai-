7677a4a33650888ed82c12319d38611130fcf25c
Kartikeyagupta5 <kartikeyagupta998@gmail.com>
2024-09-22 14:46:47 +0530

    update

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="md:text-5xl font-bold flex text-center items-center justify-center gap-4 text-3xl" > Buy Me a Chai <span><img width={44} src="/tea.gif" alt="" /></span></div>
         <p>Raise funds for your favourite creater Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure possimus voluptas esse !</p>
         <div>
-          <Link href={"/login"}>
+          <Link href={"/Login"}>
           <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Now</button></Link>
 
           <Link href={"/about"}>
